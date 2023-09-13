b6c40c5f5e629da5eeac0da9ffd06a5c32801414
Zeeshan Ul Hassan Dar <zeeshan.dar@prisjakt.nu>
2023-09-14 01:16:42 +0200

    Updated metadata

--- terraform/backend.tf
+++ terraform/backend.tf
@@ -256,12 +256,19 @@ EOT
 
 resource "aws_security_group" "allow_alb" {
   name        = "allow_docker_traffic_to_alb_sg"
-  description = "Allow all inbound traffic"
+  description = "Allow all inbound and outbound traffic"
 
   ingress {
     from_port   = 0
     to_port     = 65535
     protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  egress {
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1" # This means all protocols
     cidr_blocks = ["0.0.0.0/0"]
   }
 }
