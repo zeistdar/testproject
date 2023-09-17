f2f774b2c24b69af673ba2c9a72c72dbd92fd118
Zeeshan Ul Hassan Dar <zeeshan.dar@prisjakt.nu>
2023-09-18 01:00:11 +0200

    lambda

--- terraform/second_lambda.tf
+++ terraform/second_lambda.tf
@@ -51,8 +51,8 @@ resource "aws_lambda_function" "process_csv" {
   handler       = "index.handler"
   runtime       = "python3.11"
 
-  filename = "../lambda.zip"
-  source_code_hash = filebase64sha256("../lambda.zip")
+  filename = "/tmp/lambda.zip"
+  source_code_hash = filebase64sha256("/tmp/lambda.zip")
   timeout = 300
   memory_size   = 512
   environment {
